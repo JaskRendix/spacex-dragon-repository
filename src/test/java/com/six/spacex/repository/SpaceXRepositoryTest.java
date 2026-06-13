78ad059664d466c6768758b98809727a970914c4
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-06-13 13:39:01 +0200

    add SpaceXRepositoryTest.java

---
+++ src/test/java/com/six/spacex/repository/SpaceXRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.six.spacex.repository;
+
+import org.junit.jupiter.api.Test;
+
+class SpaceXRepositoryTest {
+
+    @Test
+    void placeholder() {
+        // TODO: add tests
+    }
+}
