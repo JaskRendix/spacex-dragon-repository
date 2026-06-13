695db595256d9db5ff32071014e9cb2c6f526296
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-06-13 13:45:36 +0200

    add tests

--- src/test/java/com/six/spacex/repository/SpaceXRepositoryTest.java
+++ src/test/java/com/six/spacex/repository/SpaceXRepositoryTest.java
@@ -1,11 +1,44 @@
 package com.six.spacex.repository;
 
+import com.six.spacex.model.*;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SpaceXRepositoryTest {
 
     @Test
-    void placeholder() {
-        // TODO: add tests
+    void shouldAssignRocketToMissionSuccessfully() {
+        SpaceXRepository repo = new SpaceXRepository();
+        repo.addRocket("Dragon1");
+        repo.addMission("MarsMission");
+
+        repo.assignRocketToMission("Dragon1", "MarsMission");
+
+        assertEquals(RocketStatus.IN_SPACE, repo.getRocket("Dragon1").getStatus());
+        assertEquals(1, repo.getMission("MarsMission").getAssignedRockets().size());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAssigningAlreadyAssignedRocket() {
+        SpaceXRepository repo = new SpaceXRepository();
+        repo.addRocket("Dragon1");
+        repo.addMission("MissionA");
+        repo.addMission("MissionB");
+
+        repo.assignRocketToMission("Dragon1", "MissionA");
+
+        assertThrows(IllegalStateException.class, () -> 
+            repo.assignRocketToMission("Dragon1", "MissionB")
+        );
+    }
+
+    @Test
+    void shouldThrowWhenAssigningRocketToNonExistingMission() {
+        SpaceXRepository repo = new SpaceXRepository();
+        repo.addRocket("Dragon1");
+
+        assertThrows(IllegalArgumentException.class, () ->
+            repo.assignRocketToMission("Dragon1", "NoSuchMission")
+        );
     }
 }
